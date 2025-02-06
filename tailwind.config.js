c54c1cc50918bc23635743d8d7a64d231af1802a
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2025-02-05 22:17:46 -0300

    cambios2

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'bg-hero': "url('./public/bg-hero.webp')",
+        'bg-hero': "url('./bg-hero.webp')",
       },
       colors: {
         text: '#140d0d',
