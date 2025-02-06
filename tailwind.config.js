0505c505aaa25bae23ef868bbae5e394cab335ee
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2025-02-05 22:16:20 -0300

    cambios1

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,21 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'bg-hero': "url('/src/assets/bg-hero.webp')",
+        'bg-hero': "url('./public/bg-hero.webp')",
       },
       colors: {
         text: '#140d0d',
         background: '#FAF9F6',
-        darkbg: '#1d1c1c',        
+        darkbg: '#1d1c1c',
         primary: '#a60827',
         secondary: '#487030',
         accent: '#bacc67',
         gold: '#FFD700',
         darkgold: "#B69D74",
       },
-      fontFamily: {        
+      fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
-        slab: ['Roboto Slab Variable', 'serif']        
+        slab: ['Roboto Slab Variable', 'serif']
       },
     },
   },
