75da5d8ef7b0bb8e80cd250d1f4fb2b25176aa60
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2025-03-24 19:28:02 -0300

    fix form and whatsapp

--- src/scripts/formHandler.js
+++ src/scripts/formHandler.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
         };
 
         try {
-            const response = await fetch("http://localhost:3000/api/contact", {
+            const response = await fetch("https://rmg-backmail.vercel.app/api/mail/", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
